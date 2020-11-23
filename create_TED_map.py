95eb671cba7f9e0f4bad625366bf4f6655ed1f5e
ericberlow <ericberlow@gmail.com>
2020-11-23 14:50:50 -0800

    updated ted talk map creation file
    added html-rich snapshot descriptions, added images to nodes, added snapshot screenshots in dropbox

--- create_TED_map.py
+++ create_TED_map.py
@@ -26,23 +26,46 @@ link_attr_map = {"source": "Source", "target": "Target", "isDirectional": "isDir
 # create some snapshots
 # snapshot - scatterplot
 sn1 = create_snapshot(
-    name="keyword themes",
-    subtitle="another scatterplot",
-    summaryImg="https://placekitten.com/220/160",
-    description="A kitten is a <b>juvenile cat</b>. After being born, kittens display primary altriciality and are totally dependent on their mother for survival. They do not normally open their eyes until after seven to ten days. After about two weeks, kittens quickly develop and begin to explore the world outside the nest. After a further three to four weeks, they begin to eat solid food and grow adult teeth. Domestic kittens are highly social animals and usually enjoy human companionship.",
+    name="Keyword Themes",
+    subtitle="Clusters of talks linked by shared keywords.",
+    summaryImg="https://www.dl.dropboxusercontent.com/s/d9vdgqouelb2oq7/Screen%20Shot%202020-11-23%20at%202.45.08%20PM.png?dl=0",
+    description="<p>Talks are linked if they have high overlap in their keywords. Similar talks self-cluster into <b>&#34;Keyword Themes&#34;</b>\
+                - or groups of talks that tend to co-share similar tags. These clusters are auto-labeled by the 3 most commonly shared tags in the group.  \
+                If you hover over or select a talk on the map, it's top most similar talks are also highlighted. </p><p><i>Colored by</i> \
+                - Keyword Theme</p><p><i>Sized by</i> - Total Views</p><p><i>How to Navigate this Network:</i></p><\
+                ul><li>Click on any node to see more details about that talk and watch the video. </li>\
+                <li>Click the 'reset' button to clear any selection.</li>\
+                <li>Use the <b>Slides</b> panel to navigate between views.</li>\
+                <li>Use the <b>Filters</b> panel to select talks by any combination of traits (tags, views, event, talk duration, year published, etc).</li>\
+                <li>Use the <b>Subset</b> button to restrict the data to the selected talks. The <b>Filters</b> panel will then summarize that <b>subset</b>. </li>\
+                <li>Use the <b>List</b> panel to see selected talks as a sortable list - and to explore their details one by one.  </li></ul>",
     layout_params={
         "plotType": "scatterplot",
         "xaxis": "OriginalX",
         "yaxis": "OriginalY",
-        "settings": {"nodeSizeAttr": "popularity index", "nodeColorAttr": "keyword theme"},
+        "settings": {"nodeSizeAttr": "popularity index", 
+                     "nodeColorAttr": "keyword theme",
+                     "nodeImageShow": True,
+                     "nodeImageAttr": "photo",
+                     "xAxShow": False,
+                     "yAxShow": False,
+                     },
     },
 )
 
 # snapshot - scatterplot
 sn2 = create_snapshot(
     name="popularity by year",
-    subtitle="scatterplot",
-    summaryImg="https://placekitten.com/220/150",
+    subtitle="See which talks were most popular in a given year.",
+    summaryImg="https://www.dl.dropboxusercontent.com/s/uepgmm7mwi5mxtj/Screen%20Shot%202020-11-23%20at%202.49.03%20PM.png?dl=0",
+    description= "<p>Talks are sorted horizontally by date published and vertically by &#34;popularity&#34;. \
+                Since some topics (e.g. pop psychology or business leadership) are more inherently more popular than others (e.g. physics), \
+                the views of each talk are indexed relative to the median views of its keyword theme. \
+                Talks above the zero line had more views than expected relative to the typical talk of its keyword theme. \
+                If you hover on or select a talk, the links display other talks with the most similar keywords. \
+                If you hover over Keyword Theme in the <b>Legend</b>, or any tags in the <b>Filters</b>, \
+                you can see the popularity trends for that theme or tag over time. </p><p><br/></p>\
+                <p><i>Colored by:  </i>Keyword Theme</p><p><i>Sized by: </i>Total Views</p>",
     layout_params={
         "plotType": "scatterplot",
         "xaxis": "year published",
