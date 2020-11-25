448e5789850f1e90ab95acc00e1b6b9f3f225595
ericberlow <ericberlow@gmail.com>
2020-11-25 07:31:26 -0800

    added more settings to the snapshots

--- create_TED_map.py
+++ create_TED_map.py
@@ -24,11 +24,11 @@ link_attr_map = {"source": "Source", "target": "Target", "isDirectional": "isDir
 
 # create some snapshots
 # snapshot - scatterplot
-'''
+
 sn1 = create_snapshot(
     name="Keyword Themes",
     subtitle="Clusters of talks linked by shared keywords.",
-    summaryImg="https://www.dl.dropboxusercontent.com/s/d9vdgqouelb2oq7/Screen%20Shot%202020-11-23%20at%202.45.08%20PM.png?dl=0",
+    summaryImg="https://www.dl.dropboxusercontent.com/s/oocyibrvojcmieg/Screen%20Shot%202020-11-25%20at%207.20.12%20AM.png?dl=0",
     description="<p>Talks are linked if they have high overlap in their keywords. Similar talks self-cluster into <b>&#34;Keyword Themes&#34;</b>\
                 - or groups of talks that tend to co-share similar tags. These clusters are auto-labeled by the 3 most commonly shared tags in the group.  \
                 If you hover over or select a talk on the map, it's top most similar talks are also highlighted. </p><p><i>Colored by</i> \
@@ -41,39 +41,79 @@ sn1 = create_snapshot(
                 <li>Use the <b>List</b> panel to see selected talks as a sortable list - and to explore their details one by one.  </li></ul>",
     layout_params={
         "plotType": "scatterplot",
-        "xaxis": "OriginalX",
-        "yaxis": "OriginalY",
+        "xaxis": "x_tsne",
+        "yaxis": "y_tsne",
         "settings": {
-            "nodeSizeAttr": "popularity_index",
+            # snode ize
+            "nodeSizeAttr": "views",
+            "nodeSizeScaleStrategy": "log", # "linear" or "log"
+            "nodeSizeMin": 1,
+            "nodeSizeMax": 20,
+            "nodeSizeMultiplier": .5,
+            "bigOnTop": True,
+            # node color and images
             "nodeColorAttr": "keyword_theme",
             "nodeImageShow": True,
             "nodeImageAttr": "photo",
+            # link rendering
+            "drawEdges": False,
+            "edgeCurvature": 0.6,
+            "edgeDirectionalRender": "outgoing",
+            "edgeSizeStrat": "attr", # or "fixed"
+            "edgeSizeAttr": "weight", # size by similarity
+            "edgeSizeMultiplier": .7,
+            # labels
+            "drawGroupLabels": True,
+            # layout rendering
             "xAxShow": False,
             "yAxShow": False,
+            "scatterAspect": 0.4, # shigher than 0.5 spreads out the scatterplot horizontally
         },
     },
 )
-'''
+
 # snapshot - scatterplot
 sn2 = create_snapshot(
     name="popularity by year",
     subtitle="See which talks were most popular in a given year.",
-    summaryImg="https://www.dl.dropboxusercontent.com/s/uepgmm7mwi5mxtj/Screen%20Shot%202020-11-23%20at%202.49.03%20PM.png?dl=0",
+    summaryImg="https://www.dl.dropboxusercontent.com/s/4v0kkfjkjmjn5zz/Screen%20Shot%202020-11-25%20at%207.19.54%20AM.png?dl=0",
     description="<p>Talks are sorted horizontally by date published and vertically by &#34;popularity&#34;. \
                 Since some topics (e.g. pop psychology or business leadership) are more inherently more popular than others (e.g. physics), \
                 the views of each talk are indexed relative to the median views of its keyword theme. \
                 Talks above the zero line had more views than expected relative to the typical talk of its keyword theme. \
                 If you hover on or select a talk, the links display other talks with the most similar keywords. \
-                If you hover over Keyword Theme in the <b>Legend</b>, or any tags in the <b>Filters</b>, \
+                If you hover over Keyword Theme or any other tags in the <b>Filters</b>, \
                 you can see the popularity trends for that theme or tag over time. </p><p><br/></p>\
                 <p><i>Colored by:  </i>Keyword Theme</p><p><i>Sized by: </i>Total Views</p>",
     layout_params={
         "plotType": "scatterplot",
-        "xaxis": "year_published",
+        "xaxis": "date_published",
         "yaxis": "popularity_index",
         "settings": {
-            "nodeSizeAttr": "popularity_index", 
-            "nodeColorAttr": "year_published"
+            # node sizing
+            "nodeSizeAttr": "views", 
+            "nodeSizeScaleStrategy": "linear", # "linear" or "log"
+            "nodeSizeMin": 5,
+            "nodeSizeMax": 10,
+            "nodeSizeMultiplier": 1,
+            # node color and images
+            "nodeColorAttr": "year_published",
+            "nodeImageShow": True,
+            "nodeImageAttr": "photo",
+            # link rendering
+            "drawEdges": False,
+            "edgeCurvature": 0.6,
+            "edgeDirectionalRender": "outgoing",
+            "edgeSizeStrat": "attr", # or "fixed"
+            "edgeSizeAttr": "weight", # size by similarity
+            "edgeSizeMultiplier": .7,
+            # labels
+            "drawGroupLabels": False, # don't show color-by labels
+            # layout rendering
+            "xAxShow": True,
+            "yAxShow": True,
+            "scatterAspect": 0.6, # shigher than 0.5 spreads out the scatterplot horizontally
+            
         },
     },
 )
@@ -94,19 +134,19 @@ create_map(
     nodeAttrsFile,
     node_attr_map,
     link_attr_map,
-    snapshots=[sn2],# sn2, sn3],
+    snapshots=[sn1,sn2], #,sn3],
     playerSettings={
         "modalTitle": "10 years of TED talks",
-        "modalDescription": '<h6>This is a map of every talk on TED.com published from 2007 to 2017.  \
+        "modalSubtitle": '<h6>This is a map of every talk on TED.com published from 2007 to 2017.  \
                             Keyword tags for each talk were enhanced by searching through the full transcript of each talk \
-                            for the presence of any keyword from the TED\'s tag list, and adding it as a tag if was not already present. \
-                            </h6><h6>Talks are linked if they have high overlap in their tags,  and they self-cluster into groups that \
-                            tend to share similar *combinations* of tags. These Keyword Themes are auto-labeled by the 3 most commonly shared tags in the group. \
-                            </h6>&#10;&#10;<h6>Data are from a public dataset on <a href="https://www.kaggle.com/rounakbanik/ted-talks/data " target="_blank">Kaggle</a>. \
+                            for the presence of any keyword from the TED\'s tag list, and adding it as a tag if was not already present.</h6>\
+                            <h6>Talks are linked if they have high overlap in their tags,  and they self-cluster into groups that \
+                            tend to share similar *combinations* of tags. These Keyword Themes are auto-labeled by the 3 most commonly shared tags in the group.</h6>\
+                            &#10;&#10;<h6>Data are from a public dataset on <a href="https://www.kaggle.com/rounakbanik/ted-talks/data " target="_blank">Kaggle</a>. \
                             <span>The network was generated using the open source python </span><a href="https://github.com/foodwebster/Tag2Network" target="_blank">\'tag2network\'</a><span> \
                             package, and visualized using <a href="http://openmappr.org" target="_blank">\'openmappr\'</a> - an open source network exploration tool. \
                             <br/></span></h6><h6><p><br/></p><p><i>This visualization is not optimized for mobile viewing and works best in Chrome browsers.   </i><br/></p></h6>',
-        "modalSubtitle": "<h5><span>How to Navigate this Network:</span><br/></h5><ul><li>Click on any node to see more details about that talk and watch the video. \
+        "modalDescription": "<h5><span>How to Navigate this Network:</span><br/></h5><ul><li>Click on any node to see more details about that talk and watch the video. \
                             </li><li>Click the '<b>reset</b>' button to clear any selection.</li><li>Use the <b>Snapshots</b> panel to navigate between views.</li>\
                             <li>Use the <b>Filters</b> panel to select talks by any combination of traits (tags, views, event, talk duration, year published, etc).</li>\
                             <li>Click the '<b>Subset</b>' button to restrict the data to the selected nodes. The <b>Filters</b> panel will then summarize that <b>subset</b>. </li>\
