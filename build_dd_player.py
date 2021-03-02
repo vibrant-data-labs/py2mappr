5eb7cb80f74abe5297efe267530d030dc324d66b
ericberlow <ericberlow@gmail.com>
2021-03-02 05:51:48 -0800

    test project

--- build_dd_player.py
+++ build_dd_player.py
@@ -16,14 +16,14 @@ import webbrowser
 
 # configure the files and folders
 wd = pl.Path.cwd()
-projectPath = wd /"projects"/"digital_delta"
-inDataPath = projectPath/"DigitalDelta_data_in"
+projectPath = wd /("test_projects/digital_delta")
+inDataPath = projectPath/"data_in"
 
 nodesFile = inDataPath /"nodes.csv"
 linksFile = inDataPath /"links.csv"
 nodeAttrsFile = inDataPath/"node_attrs.csv"
-projectFolder = "DigitalDelta_player"
-outFolder = projectPath/projectFolder
+outFolder = projectPath/"data_out"
+# TODO: ?? linkAttrsFile = inDataPath/"link_attrs.csv"  ??? 
 
 
 
@@ -37,78 +37,183 @@ link_attr_map = {"source": "Source", "target": "Target", "isDirectional": "isDir
 # snapshot - scatterplot
 
 sn1 = create_snapshot(
-    name="Causal Clusters",
-    subtitle="blah blah",
-    summaryImg="https://www.dl.dropboxusercontent.com/s/oocyibrvojcmieg/Screen%20Shot%202020-11-25%20at%207.20.12%20AM.png?dl=0",
-    description="<p>Talks are linked if they have high overlap in their keywords. Similar talks self-cluster into <b>&#34;Keyword Themes&#34;</b>\
-                - or groups of talks that tend to co-share similar tags. These clusters are auto-labeled by the 3 most commonly shared tags in the group.  \
-                If you hover over or select a talk on the map, it's top most similar talks are also highlighted. </p><p><i>Colored by</i> \
-                - Keyword Theme</p><p><i>Sized by</i> - Total Views</p><p><i>How to Navigate this Network:</i></p><\
-                ul><li>Click on any node to see more details about that talk and watch the video. </li>\
-                <li>Click the 'reset' button to clear any selection.</li>\
-                <li>Use the <b>Slides</b> panel to navigate between views.</li>\
-                <li>Use the <b>Filters</b> panel to select talks by any combination of traits (tags, views, event, talk duration, year published, etc).</li>\
-                <li>Use the <b>Subset</b> button to restrict the data to the selected talks. The <b>Filters</b> panel will then summarize that <b>subset</b>. </li>\
-                <li>Use the <b>List</b> panel to see selected talks as a sortable list - and to explore their details one by one.  </li></ul>",
+    name="Consensus Network",
+    
+    subtitle="Root causes linked by what affects what",  
+    summaryImg="https://www.dl.dropboxusercontent.com/s/t1idhxri5j0lihj/Screen%20Shot%202021-03-01%20at%209.16.13%20AM.png?dl=0",
+    description="<p>Each node is a root cause and they are linked by what influences what.\
+                </p><p><b>Node Size</b> - Total number of links</p><p><b>Node</b> <b>Color</b> - \
+                Broad topic of the root cause.</p><p><b>Links</b><span> -  \
+                Consensus of at least a 3 votes that one root cause has a strong influence on another, \
+                and in the same way (positive vs negative). </span></p>\
+                <p><b>Link direction </b><span>is</span><i> <b>clockwise influence from source to the target. </b></i></p>",
     layout_params={
         "plotType": "scatterplot",
         "xaxis": "x_tsne",
         "yaxis": "y_tsne",
+        
         "settings": {
             # node size
-            "nodeSizeAttr": "DEGREE",
+            "nodeSizeAttr": "Degree",
             "nodeSizeScaleStrategy": "linear", # "linear" or "log"
-            "nodeSizeMin": 1,
-            "nodeSizeMax": 20,
-            "nodeSizeMultiplier": .5,
-            "bigOnTop": True,
+            "nodeSizeMin": 3,
+            "nodeSizeMax": 15,
+            "nodeSizeMultiplier": 1.3,
+            "bigOnTop": False,
             # node color and images
-            "nodeColorAttr": "Causal Cluster",
+            "nodeColorAttr": "Category",
+            "nodeColorPaletteOrdinal": [
+                        {
+                            "col": "#61beac"
+                        },
+                        {
+                            "col": "#f7e05f"
+                        },
+                        {
+                            "col": "#beebf4"
+                        },
+                        {
+                            "col": "#fbc9bb"
+                        },
+                        {
+                            "col": "#d97c51"
+                        },
+                        {
+                            "col": "#8f8e8f"
+                        },
+                        {
+                            "col": "#84a484"
+                        },
+                        {
+                            "col": "#b23333"
+                        },
+                        {
+                            "col": "#994c00"
+                        },
+                        {
+                            "col": "#0073bf"
+                        },
+                        {
+                            "col": "#bf9900"
+                        },
+                        {
+                            "col": "#4c9999"
+                        },
+                        {
+                            "col": "#739900"
+                        },
+                        {
+                            "col": "#732673"
+                        }
+                    ],
             "nodeImageShow": False,
             "nodeImageAttr": "",
             # link rendering
             "drawEdges": True,
             "edgeCurvature": 0.6,
             "edgeDirectionalRender": "all",
-            "edgeSizeStrat": "fixed", # or "attr"
-            "edgeSizeAttr": "votes", # size by similarity
-            "edgeSizeMultiplier": .7,
+            "edgeSizeStrat": "fixed", #  "attr" // "fixed"
+            "edgeSizeAttr": "votes", # size by 
+            "edgeColorStrat": "gradient",  # source / target / gradient / attr / select
+            "edgeColorAttr": "OriginalColor",
+            "edgeSizeMultiplier": 1,
+            # neighbor rendering
+            "nodeSelectionDegree": 1,
             # labels
             "drawGroupLabels": True,
             # layout rendering
             "xAxShow": False,
             "yAxShow": False,
-            "scatterAspect": 0.4, # shigher than 0.5 spreads out the scatterplot horizontally
+            "scatterAspect": 0.5, # shigher than 0.5 spreads out the scatterplot horizontally
+            "savedZoomLevel": -1,
         },
     },
 )
 
 # snapshot - scatterplot
 sn2 = create_snapshot(
-    name="reach vs leverage",
-    subtitle=".",
-    summaryImg="https://www.dl.dropboxusercontent.com/s/4v0kkfjkjmjn5zz/Screen%20Shot%202020-11-25%20at%207.19.54%20AM.png?dl=0",
-    description="<p>Talks are sorted horizontally by date published and vertically by &#34;popularity&#34;. \
-                Since some topics (e.g. pop psychology or business leadership) are more inherently more popular than others (e.g. physics), \
-                the views of each talk are indexed relative to the median views of its keyword theme. \
-                Talks above the zero line had more views than expected relative to the typical talk of its keyword theme. \
-                If you hover on or select a talk, the links display other talks with the most similar keywords. \
-                If you hover over Keyword Theme or any other tags in the <b>Filters</b>, \
-                you can see the popularity trends for that theme or tag over time. </p><p><br/></p>\
-                <p><i>Colored by:  </i>Keyword Theme</p><p><i>Sized by: </i>Total Views</p>",
+    name="Catalysts",
+    subtitle="Catalyst factors and their relative position in the problem hierarchy",
+    summaryImg="https://www.dl.dropboxusercontent.com/s/ps973fk5pbsfhs9/Screen%20Shot%202021-03-01%20at%206.58.06%20PM.png?dl=0",
+    description="<p><b>Node Color</b> - &#34;Top Catalysts&#34; - yellow nodes were in the top 75th percentile of catalyst scores \
+                on average over 1000 random link deletion trials.</p><p><b>Node Size</b> - &#34;Catalyst Score&#34; - \
+                larger nodes have both high leverage and high reach. These are root cause which have few others that influence them, \
+                but if solved they have high primary and secondary effects on the rest of the network.  </p>\
+                <p><b>Link Color</b> - Grey links are positive (an increase in the source leads to an increase in the target). \
+                Red links are negative (an increase in the source leads to a decrease in the target). \
+                <b>Link Direction</b><span> is </span><i>clockwise from source to the target. </i></p>\
+                <p><b>&#34;Upstream &lt;&lt;--&gt;&gt; Downstream&#34;</b> is the relative position in the causal flow hierarchy\
+                of the network.  Nodes on the far right tend to be the 'downstream' recipients of long chains of influences, \
+                while nodes at the far left are more 'upstream' with shorter average incoming influence chains. \
+                <br/><span><b>&#34;Average Catalyst Score&#34;</b> is a keystone index of 2 Degree Reach  and Leverage. \
+                Nodes at the top are root factors which, relative to others have fewer root factors that influence them but \
+                if solved they have high primary and secondary influence on the rest of the network. \
+                Note that most of the top Catalysts tend to be 'upstream' problems in the causal hierarchy of the network.</span></p>\
+                <p><span>To account for known error in link over-counting, we randomly thinned the network 1,000 times to \
+                20% connected (from 38%). Each metric shown here is the average score across all 1,000 sampled networks.  </span><br/></p><p></p>",
     layout_params={
         "plotType": "scatterplot",
-        "xaxis": "leverage_pctl_mean",
-        "yaxis": "reach_mean",
+        "xaxis": "Upstream <<-->> Downstream (Pctl)",
+        "yaxis": "Average Keystone Score",
+        "camera": {
+                "normalizeCoords": True,
+                "x": 0,
+                "y": 43.9873417721519,
+                "r": 1.5
+            },
         "settings": {
             # node sizing
-            "nodeSizeAttr": "keystone_mean", 
+            "nodeSizeAttr": "Average Keystone Score", 
             "nodeSizeScaleStrategy": "linear", # "linear" or "log"
-            "nodeSizeMin": 2,
-            "nodeSizeMax": 10,
-            "nodeSizeMultiplier": 1,
+            "nodeSizeMin": 3,
+            "nodeSizeMax": 20,
+            "nodeSizeMultiplier": 1.3,
             # node color and images
-            "nodeColorAttr": "Keystone",
+            "nodeColorAttr": "Top Keystones (75pctl)",
+            "nodeColorPaletteOrdinal": [
+                        {
+                            "col": "#61beac"
+                        },
+                        {
+                            "col": "#f7e05f"
+                        },
+                        {
+                            "col": "#beebf4"
+                        },
+                        {
+                            "col": "#fbc9bb"
+                        },
+                        {
+                            "col": "#d97c51"
+                        },
+                        {
+                            "col": "#8f8e8f"
+                        },
+                        {
+                            "col": "#84a484"
+                        },
+                        {
+                            "col": "#b23333"
+                        },
+                        {
+                            "col": "#994c00"
+                        },
+                        {
+                            "col": "#0073bf"
+                        },
+                        {
+                            "col": "#bf9900"
+                        },
+                        {
+                            "col": "#4c9999"
+                        },
+                        {
+                            "col": "#739900"
+                        },
+                        {
+                            "col": "#732673"
+                        }
+                    ],
             "nodeImageShow": False,
             "nodeImageAttr": "",
             # link rendering
@@ -118,12 +223,26 @@ sn2 = create_snapshot(
             "edgeSizeStrat": "fixed", # or "attr"
             "edgeSizeAttr": "votes", # size by similarity
             "edgeSizeMultiplier": .7,
+            "edgeColorStrat": "attr",  # source / target / gradient / attr / select
+            "edgeColorAttr": "isNeg",
+            "edgeColorPaletteNumeric": [
+                        {
+                            "col": "#828282"
+                        },
+                        {
+                            "col": "#d34545"
+                        }
+                    ],
+            # neighbor rendering
+            "nodeSelectionDegree": 0,
             # labels
-            "drawGroupLabels": False, # don't show color-by labels
+            "drawGroupLabels": False, # cluster labels
             # layout rendering
             "xAxShow": True,
             "yAxShow": True,
-            "scatterAspect": 0.6, # shigher than 0.5 spreads out the scatterplot horizontally
+            "scatterAspect": 0.5, # shigher than 0.5 spreads out the scatterplot horizontally
+            # node right panel
+            "nodeFocusShow": False,
             
         },
     },
@@ -147,21 +266,28 @@ create_map(
     link_attr_map,
     snapshots=[sn1,sn2], #,sn3],
     playerSettings={
-        "modalTitle": "10 years of TED talks",
-        "modalSubtitle": '<h6>This is a map of every talk on TED.com published from 2007 to 2017.  \
-                            Keyword tags for each talk were enhanced by searching through the full transcript of each talk \
-                            for the presence of any keyword from the TED\'s tag list, and adding it as a tag if was not already present.</h6>\
-                            <h6>Talks are linked if they have high overlap in their tags,  and they self-cluster into groups that \
-                            tend to share similar *combinations* of tags. These Keyword Themes are auto-labeled by the 3 most commonly shared tags in the group.</h6>\
-                            &#10;&#10;<h6>Data are from a public dataset on <a href="https://www.kaggle.com/rounakbanik/ted-talks/data " target="_blank">Kaggle</a>. \
-                            <span>The network was generated using the open source python </span><a href="https://github.com/foodwebster/Tag2Network" target="_blank">\'tag2network\'</a><span> \
-                            package, and visualized using <a href="http://openmappr.org" target="_blank">\'openmappr\'</a> - an open source network exploration tool. \
-                            <br/></span></h6><h6><p><br/></p><p><i>This visualization is not optimized for mobile viewing and works best in Chrome browsers.   </i><br/></p></h6>',
-        "modalDescription": "<h5><span>How to Navigate this Network:</span><br/></h5><ul><li>Click on any node to see more details about that talk and watch the video. \
-                            </li><li>Click the '<b>reset</b>' button to clear any selection.</li><li>Use the <b>Snapshots</b> panel to navigate between views.</li>\
-                            <li>Use the <b>Filters</b> panel to select talks by any combination of traits (tags, views, event, talk duration, year published, etc).</li>\
-                            <li>Click the '<b>Subset</b>' button to restrict the data to the selected nodes. The <b>Filters</b> panel will then summarize that <b>subset</b>. </li>\
-                            <li>Use the <b>List</b> panel to see selected or subsetted talks as a sortable list - and to explore their details one by one by clicking on them.  </li></ul>",
+        "startPage": "filter", #filter // snapshots // list // legend // splash?
+        "headerTitle": "Ditigal Delta",
+        "modalTitle": "Digital Delta",
+        "headerImageUrl": "https://www.dl.dropboxusercontent.com/s/jpwr6oynmy5q9y4/Screen%20Shot%202021-01-28%20at%208.18.28%20AM.png?dl=0",
+        "modalSubtitle": '<h3>Making Digital Experiences More Beautiful for Young People</h3><p>Digital Delta is a crowdsourced research \
+                        project working to discover the key opportunities that impact youth wellbeing in digital places and experiences. </p>\
+                        <p>This is a free and public tool for your use. Digital Delta crowdsourced its data from over 800 community members, \
+                        56% under 25 years old. We analyzed how 77 root factors influenced each other that created a network with a clear \
+                        structure to better understand this complicated problem.  The analysis revealed  3 opportunity areas comprised of 13 \
+                        catalysts with the best chances of creating more beautiful digital experiences for youth as they grow up.</p>\
+                        <p>Now, together with our partners and all of you, we are working toward that transformation. \
+                        We encourage you to explore and play with the data through Open Mapper. You can dive into our process and outcomes \
+                        we found on the Digital Delta Website. </p><p><br/></p>',
+        "modalDescription": "<h3>How to Navigate this Network:</h3><ul><li>Click on any node to to see more details about it. </li>\
+                        <li>Click the whitespace or '<b>Reset</b>' button to clear any selection.</li><li>Use the <b>Filters</b> panel \
+                        to select nodes by any  combination of tags. </li><li>Click the <b>'Apply'</b> button to subset the data to \
+                        the selected nodes - The <b>Filters</b> panel will then show a summary of that subset.<br/></li>\
+                        <li>Use the <b>List</b> panel to see a sortable list of any nodes selected or subset. \
+                        You can also browse their details one by one by clicking on them in the list.</li>\
+                        <li>Use the Snapshots panel to navigate between views.</li></ul><p><br/></p><p>\
+                        This visualization is not optimized for mobile devices and is best viewed in Chrome or Safari browsers\ \
+                        on the laptop/desktop. </p>&#10;",
     },
     outFolder=outFolder,
 )
