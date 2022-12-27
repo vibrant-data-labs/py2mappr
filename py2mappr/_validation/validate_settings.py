10c09c9dbe59ba920fb75f97e08d1d0e38ff94ea
mike.tulubaev <mneogoogacc@gmail.com>
2022-12-27 19:44:50 +0400

    added exception handling in schema validation

--- py2mappr/_validation/validate_settings.py
+++ py2mappr/_validation/validate_settings.py
@@ -6,10 +6,13 @@ from .warn import warn
 def validate_settings(settings: Dict):
     # shared settings file
     settings_url = "https://raw.githubusercontent.com/vibrant-data-labs/openmappr-player/master/settings.schema.json"
-    schema = requests.get(settings_url).json()
+    try:
+        schema = requests.get(settings_url).json()
 
-    validator = jsonschema.Draft4Validator(schema)
+        validator = jsonschema.Draft4Validator(schema)
 
-    errors = validator.iter_errors(settings)
-    for error in errors:
-        warn(f"[Invalid Schema]: {error.message} at {error.json_path}")
+        errors = validator.iter_errors(settings)
+        for error in errors:
+            warn(f"[Invalid Schema]: {error.message} at {error.json_path}")
+    except:
+        warn(f"Unable to fetch settings schema at {settings_url}, skipping validation")
