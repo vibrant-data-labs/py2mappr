7d7b62e2a1d1e1d3ef5e720648a02084843fa96f
mike.tulubaev <mneogoogacc@gmail.com>
2023-02-07 20:08:46 +0500

    DEV-171 switched to use pytest

--- tests/_builder/test_build_dataset.py
+++ tests/_builder/test_build_dataset.py
@@ -1,44 +1,43 @@
-from unittest import TestCase
 import pandas as pd
 
 from py2mappr._builder.build_dataset import build_datapoints
 
 
-class BuildDatapointTestCase(TestCase):
-    def test_string_text_is_converted_from_md(self):
-        datapoint_dict = {
-            "id": "test",
-            "description": "This is a **test**.",
-        }
+def test_string_text_is_converted_from_md():
+    datapoint_dict = {
+        "id": "test",
+        "description": "This is a **test**.",
+    }
 
-        datapoint = pd.Series(datapoint_dict)
-        dpAttribTypes = {"id": "string", "description": "string"}
-        dpRenderTypes = {"id": "default", "description": "text"}
-        expected = "<p>This is a <strong>test</strong>.</p>"
+    datapoint = pd.Series(datapoint_dict)
+    dpAttribTypes = {"id": "string", "description": "string"}
+    dpRenderTypes = {"id": "default", "description": "text"}
 
-        result = build_datapoints(
-            pd.DataFrame([datapoint], index=["description"]),
-            dpAttribTypes,
-            dpRenderTypes,
-        )
+    result = build_datapoints(
+        pd.DataFrame([datapoint], index=["description"]),
+        dpAttribTypes,
+        dpRenderTypes,
+    )
 
-        self.assertEqual(expected, result[0]["attr"]["description"])
+    expected = "<p>This is a <strong>test</strong>.</p>"
+    assert expected == result[0]["attr"]["description"]
 
-    def test_string_default_not_converted_from_md(self):
-        datapoint_dict = {
-            "id": "test",
-            "description": "This is a **test**.",
-        }
 
-        datapoint = pd.Series(datapoint_dict)
-        dpAttribTypes = {"id": "string", "description": "string"}
-        dpRenderTypes = {"id": "default", "description": "default"}
-        expected = "This is a **test**."
+def test_string_default_not_converted_from_md():
+    datapoint_dict = {
+        "id": "test",
+        "description": "This is a **test**.",
+    }
 
-        result = build_datapoints(
-            pd.DataFrame([datapoint], index=["description"]),
-            dpAttribTypes,
-            dpRenderTypes,
-        )
+    datapoint = pd.Series(datapoint_dict)
+    dpAttribTypes = {"id": "string", "description": "string"}
+    dpRenderTypes = {"id": "default", "description": "default"}
 
-        self.assertEqual(expected, result[0]["attr"]["description"])
+    result = build_datapoints(
+        pd.DataFrame([datapoint], index=["description"]),
+        dpAttribTypes,
+        dpRenderTypes,
+    )
+
+    expected = "This is a **test**."
+    assert expected == result[0]["attr"]["description"]
